c453247e7756a3918a44aacb5dbd342cce25e59a
Aaron Sikes <aaron@sikes.io>
2016-05-05 16:07:46 -0400

    When doing a Mixmax edit, draw the existing image
    Unfortunately, the way this drawing plugin works is by clearing and
    redrawing the image based on all the click events it's recorded so far.
    This means the existing image gets cleared as soon as you start editing,
    so it's not very useful.
    
    This is a limitation of the plugin. We can look for another plugin,
    extend this one, or write our own to address the issue

--- public/app.js
+++ public/app.js
@@ -1,14 +1,26 @@
 $(function() {
   // Weirdly, the function returns the element when initializing, but the
   // Sketch instance when already initialized. We want the instance.
-  $('#draw-on-this').sketch()
+
+  var params = DrawMixmax.utils.decodeParams(location.search)
+
+  var canvas = document.getElementById('draw-on-this')
+  if (params.data && params.data.src) {
+    var img = document.createElement('img')
+    img.src = params.data.src
+
+    var context = canvas.getContext('2d')
+    context.drawImage(img, 0, 0)
+  }
+
+
+  $(canvas).sketch()
 
   $('#cancel').click(function() {
     Mixmax.cancel()
   })
 
   $('#ok').click(function() {
-    var canvas = document.getElementById('draw-on-this')
     var src = canvas.toDataURL()
     Mixmax.done({
       src: src,
