e4524bc76a5c56ed7f635af61ae1ba9e41141b22
Aaron Sikes <aaron@sikes.io>
2016-05-05 13:08:40 -0400

    Remove all the giphy-specific code
    Turn the whole thing into a super-simple app that just inserts a *mic drop*

---
+++ public/app.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('#cancel').click(function() {
+    Mixmax.cancel()
+  })
+
+
+  $('#ok').click(function() {
+    Mixmax.done({
+      src: "https://m.popkey.co/793eef/edp6o.gif",
+      width: 500
+    })
+  })
+})
