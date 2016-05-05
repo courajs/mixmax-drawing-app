7358517270989dadc4df85f5d738ba4569261b7f
Aaron Sikes <aaron@sikes.io>
2016-05-05 16:51:22 -0400

    Link to sketch.js

--- public/vendor/sketch.js
+++ public/vendor/sketch.js
@@ -21,6 +21,8 @@ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+// http://intridea.github.io/sketch.js/
+
 var __slice = Array.prototype.slice;
 (function($) {
   var Sketch;
